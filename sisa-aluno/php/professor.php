e06d1d917072df42344268fdad592b6c3c33a5c1
Guilherme nunes <140085055+guilherme-nunes-x@users.noreply.github.com>
2024-04-13 16:07:32 -0300

    Add files via upload

---
+++ sisa-aluno/php/professor.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="css/inicial.css" />
+    <link
+      rel="shortcut icon"
+      href="imgs/school-svgrepo-com.svg"
+      type="image/x-icon"
+    />
+    <title>Inicial</title>
+  </head>
+  <body>
+    <section>
+      <h1>Login professor</h1>
+
+      <form action="">
+        <div id="vs">
+          <label for="">Professor</label>
+          <input type="text" name="name" placeholder="Professor" required />
+          <label for="">Senha</label>
+          <input type="password" name="senha" placeholder="Senha" required />
+        </div>
+        <div id="suv">
+        <label for="submit"></label>
+        <input type="submit" value="Entrar" id="sub" />
+    </div>
+      </form>
+    </section>
+  </body>
+</html>
