cb4d6171ca5d65639929b866aca199b00f2c85c9
Guilherme nunes <140085055+guilherme-nunes-x@users.noreply.github.com>
2024-04-23 20:32:45 -0300

    Add files via upload

---
+++ php/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="css/inicial.css" />
+    <link
+      rel="shortcut icon"
+      href="css/imgs/school-svgrepo-com.svg"
+      type="image/x-icon"
+    />
+    <title>Inicial</title>
+  </head>
+  <body>
+    <section>
+      <h1>Student System</h1>
+
+      <p>Escolha uma das opções abaixo:</p>
+
+      <a href="long.php" class="px">login</a>
+      <a href="ins.php" class="px">inscrever</a>
+      <a href="tabs.php" class="px">Tabelas</a>
+    </section>
+  </body>
+</html>
