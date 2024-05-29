7a9c69d9d7a82bcb19130c292a4b879fc263966d
guilherme-nunes-x <140085055+guilherme-nunes-x@users.noreply.github.com>
2024-05-28 21:08:13 -0300

    Correção do bug em aluno

--- sisa-aluno/php/aluno/sis_aluno.php
+++ sisa-aluno/php/aluno/sis_aluno.php
@@ -4,7 +4,7 @@ session_start();
 if((!isset($_SESSION['email']) == true) and (!isset($_SESSION['senha']) == true)){
     unset($_SESSION['email']);
     unset($_SESSION['senha']);
-    header(' location: aluno.php');
+    header('location: aluno.php');
 }
 $login = $_SESSION['email'];
 ?>
